211683781de60d5a27b75b9e6fe5d606e6bd14f5
GiantCherry <lunev1981@mail.ru>
2019-10-09 21:31:54 +0200

    Step 1. Микрофреймворк Slim.

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+// Подключение автозагрузки через composer
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome to Slim!');
+});
+$app->run();
