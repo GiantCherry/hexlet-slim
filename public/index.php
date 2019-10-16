db45f9b37645fdad7eabbcdb01f06df870627093
GiantCherry <lunev1981@mail.ru>
2019-10-16 21:58:15 +0200

    Step 6. Поисковые формы. Самостоятельная работа.

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ require __DIR__ . '/../vendor/autoload.php';
 // Список пользователей
 // Каждый пользователь – ассоциативный массив следующей структуры: id, firstName, lastName, email
 $users = App\Generator::generate(100);
+$myUsers = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -31,6 +32,29 @@ $app->get('/users', function ($request, $response) use ($users) {
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 });
 
+$app->get('/my-users', function ($request, $response) use ($myUsers) {
+    $term = $request->getQueryParam('term');
+    $searchAnswer = [];
+    foreach ($myUsers as $user) {
+        if (!empty($term)) {
+            if (strpos(mb_strtolower($user), mb_strtolower($term)) !== false) {
+                $searchAnswer [] = $user;
+            }
+        }
+    }
+    if (empty($searchAnswer)) {
+        $searchAnswer = $myUsers;
+    }
+
+    $params = [
+        'term' => $term,
+        //'users' => $users,
+        'searchAnswer' => $searchAnswer
+    ];
+
+    return $this->get('renderer')->render($response, 'users/my_users.phtml', $params);
+});
+
 $app->get('/users/{id}', function ($request, $response, $args) use ($users) {
     $id = $args['id'];
     if ($id > 0) {
@@ -38,13 +62,7 @@ $app->get('/users/{id}', function ($request, $response, $args) use ($users) {
     }
 
     $user = $users[$id];
-
-    $params = [
-        'id' => $user['id'],
-        'firstName' => $user['firstName'],
-        'lastName' => $user['lastName'],
-        'email' => $user['email']
-    ];
+    $params = ['user' => $user];
 
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
